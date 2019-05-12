28b05bb4be9977e14f1d9bc8bbb38d92758104ab
student4 <student4@pcoip.ki.agh.edu.pl>
2019-05-12 10:43:17 +0200

    Tests with get and post

--- src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
+++ src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
@@ -5,6 +5,8 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,6 +24,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 import com.company.enroller.persistence.ParticipantService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ParticipantRestController.class)
@@ -48,5 +52,56 @@ public class ParticipantRestControllerTest {
 		mvc.perform(get("/participants").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].login", is(participant.getLogin())));
 	}
+	
+	@Test
+	public void testGetParticipant() throws Exception
+	{
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+		
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+		
+		mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(new ObjectMapper().writeValueAsString(participant)));
+	}
+	
+	@Test
+	public void testAddParticipant() throws Exception
+	{
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+		
+		given(participantService.findByLogin(participant.getLogin())).willReturn(null);
+		given(participantService.add(participant)).willReturn(participant);
+		
+		mvc.perform(post("/participants").contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(participant)))
+				.andExpect(content().string(new ObjectMapper().writeValueAsString(participant)));
+		
+	}
+	
+	@Test
+	public void testTheSameParticipant() throws Exception
+	{
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+		
+		given(participantService.findByLogin(participant.getLogin())).willReturn(participant);
+		given(participantService.add(participant)).willReturn(participant);
+		
+		mvc.perform(post("/participants").contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(participant)))
+				.andExpect(content().string("Unable to create. A participant with login " + participant.getLogin() + " already exist."))
+				.andExpect(status().isConflict());
+	}
 
 }
+
+
+
+
+
+
